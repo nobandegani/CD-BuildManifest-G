8c8c6a5f82e7d8777d0077e3121d1f357333bf2d
nobandegan <76049422+nobandegan@users.noreply.github.com>
2021-04-12 02:50:22 +0430

    Create GenerateBuildManifest.bat

---
+++ GenerateBuildManifest.bat
@@ -0,0 +1,60 @@
+::Copyright Inoland Studio, Inc. All Rights Reserved.
+::support@inoland.site
+
+@echo off
+set /P filename="Enter CustomName/exam=VariantA   ?"
+if [%filename%]==[] (set filename=VariantA)
+
+set /P bid="Enter BuildID/exam=Patcher-Live   ?"
+if [%bid%]==[] (set bid=Patcher-Live)
+
+set /P vnumber="Enter Version Number/exam=ver001   ?"
+if [%vnumber%]==[] (set vnumber=ver001)
+
+set /P vpath="Enter Custom Path/exam=VariantA   ?"
+if [%vpath%]==[] (set vpath=%filename%)
+
+
+set cpatch=.\BuildManifest-%filename%.txt
+
+
+::set $NUM_ENTRIES =   -----------------------------------------------------------+
+
+set cnt=0
+for %%A in ("*.pak") do set /a cnt+=1
+set /A nentries= %cnt% - 1
+echo ^$NUM_ENTRIES =^ %nentries% >%cpatch%
+
+
+::set $BUILD_ID =   --------------------------------------------------------------
+echo ^$BUILD_ID =^ %bid% >>%cpatch%
+
+
+::set pakchunk entries   ---------------------------------------------------------
+setlocal enabledelayedexpansion
+FOR %%I IN ("*.pak") DO (
+	SET fne=%%~nxI
+	SET fn=%%~nI
+	SET chunkid=!fn:~8,4!
+
+	if !fne! NEQ pakchunk0-WindowsNoEditor.pak (echo !fne!		%%~zI		%vnumber%	!chunkid!	/%vpath%/!fne! >>%cpatch% ) 
+)
+
+
+
+
+::set var3=salamabdolah
+::echo %var3:~1,2%
+
+Pause
+::FOR %%variable IN list DO do_something
+
+::$NUM_ENTRIES = 7
+::$BUILD_ID = Patcher-Live
+::pakchunk2001-WindowsNoEditor.pak	649386902	ver001	2001	/VariantA/pakchunk2001-WindowsNoEditor.pak
+::pakchunk2002-WindowsNoEditor.pak	534713332	ver001	2002	/VariantA/pakchunk2002-WindowsNoEditor.pak
+::pakchunk2003-WindowsNoEditor.pak	752513736	ver001	2003	/VariantA/pakchunk2003-WindowsNoEditor.pak
+::pakchunk2004-WindowsNoEditor.pak	14426449	ver001	2004	/VariantA/pakchunk2004-WindowsNoEditor.pak
+::pakchunk2005-WindowsNoEditor.pak	3898957	ver001	2005	/VariantA/pakchunk2005-WindowsNoEditor.pak
+::pakchunk5001-WindowsNoEditor.pak	8005	ver001	5001	/VariantA/pakchunk5001-WindowsNoEditor.pak
+::pakchunk10001-WindowsNoEditor.pak	395255622	ver001	10001	/VariantA/pakchunk10001-WindowsNoEditor.pak
